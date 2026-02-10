d945a42b0b41494bcf4ab5981af18abb82c032b4
AI Dataset Project <user@dataset-project.local>
2026-02-10 17:11:38 +0800

    更改配置

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // 核心修改：添加这一行，必须与你的 Nginx 配置路径 /homepage/ 保持一致
+  // 注意前后都要有斜杠
+  base: '/homepage/',
+
   server: {
     port: 3000,
     open: true
